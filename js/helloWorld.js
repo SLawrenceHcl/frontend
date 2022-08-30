0c44000ef7b0c604b84c3785845ddcd5be602781
SLawrenceHcl <stephen.lawrence@hcl.com>
2022-08-30 13:00:34 -0700

    initial commit

---
+++ js/helloWorld.js
@@ -0,0 +1,13 @@
+// alert("HelloWorld!");
+
+
+function testabc(){
+    // console.log("testabc");
+    alert("HelloWorld!");
+}
+
+function hello(name){
+    var a = document.getElementById('name').value;
+    console.log("hello: "+ a);
+    alert(a);
+}
