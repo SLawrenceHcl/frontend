1d436e00c6f7b99cc19f2ebf859de4f21ecf2a2c
SLawrenceHcl <stephen.lawrence@hcl.com>
2022-09-06 13:24:59 -0700

    hamburger progress

--- js/helloWorld.js
+++ js/helloWorld.js
@@ -11,3 +11,15 @@ function hello(name){
     console.log("hello: "+ a);
     alert(a);
 }
+function alertButton(){
+    alert("Sign in prompt");
+}
+
+function hamburger() {
+    var x = document.getElementsByClassName("nav-link")
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
