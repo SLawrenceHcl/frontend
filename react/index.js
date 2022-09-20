518159f86c27cd01d767fb25ce7d2895cf00f569
SLawrenceHcl <stephen.lawrence@hcl.com>
2022-09-20 08:54:00 -0700

    checking react code

---
+++ react/index.js
@@ -0,0 +1,9 @@
+// ReactDOM.render(<h1>React Learning</h1>, document.getElementById("root"))
+
+// import ReactDOM from "react-dom";
+// import App from 'App';
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<h1>Hello, world!</h1>);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, world!</h1>);
